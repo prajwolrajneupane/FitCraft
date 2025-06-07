4d5189e85186429d0401765062fc0aef9def143a
Prajwol Raj <prajwolraj88@gmail.com>
2025-06-07 19:53:46 +0545

    euta item added

--- front/src/Wearables.jsx
+++ front/src/Wearables.jsx
@@ -1,44 +1,40 @@
 import React from "react";
-import { useParams, useLocation,Link } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 const items = [
-  { name: "T-Shirt", image: "/blackT.png" ,roll:10},
+  { name: "T-Shirt", image: "/blackT.png", roll: 10 },
   { name: "Hoodie", image: "/hoodie.png" },
   { name: "Cap", image: "/cap.png" },
   { name: "Hat", image: "/cap.png" },
   { name: "Shirt", image: "/shirt.png" },
+  { name: "Monkey Cap", image: "/monkeycap.png" },
 ];
 
 export default function Wearables() {
-    
+
   const handleSelect = (item) => {
     console.log(`Selected item: ${item.name}`);
-    
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-4xl mx-auto text-center">
-        <h1 className="text-3xl font-bold mb-6">Choose What to Design</h1>
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
+    <div className="h-screen w-full py-12 px-4 sm:px-6 lg:px-10 ">
+      <div className="max-w-3xl mx-auto text-center ">
+        <h1 className="text-3xl font-bold mb-10">Choose what to Design</h1>
+        <div className="flex flex-wrap justify-center gap-15">
           {items.map((item) => (
-                 <Link to={`/Canvas/${item.name}`} state={item}>
-
-<div
-              key={item.name}
-              onClick={() => handleSelect(item)}
-              className="cursor-pointer bg-white rounded-2xl shadow-md hover:shadow-xl transition-shadow duration-300 p-1 flex flex-col items-center"
-            >
-              <img
-                src={item.image}
-                alt={item.name}
-                className="w-28 h-30 object-contain mb-4"
-              />
-              <span className="text-lg font-medium">{item.name}</span>
-            </div>
-</Link>
-
+            <Link to={`/Canvas/${item.name}`} state={item} key={item.name}>
+              <div
+                onClick={() => handleSelect(item)}
+                className="cursor-pointer bg-white rounded-2xl shadow-md hover:shadow-xl transition-shadow duration-300 p-4 flex flex-col items-center w-40"
+              >
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="w-28 h-30 object-contain mb-4"
+                />
+                <span className="text-lg font-medium">{item.name}</span>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
